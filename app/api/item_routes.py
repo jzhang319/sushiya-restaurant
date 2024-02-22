6d6c66db3854306909e128e5ad23f1807d875cfb
jzhang319 <jzhang319@gmail.com>
2024-02-22 01:41:03 -0500

    able to parse csv file from the backend

--- app/api/item_routes.py
+++ app/api/item_routes.py
@@ -2,13 +2,25 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import User, Item
 import pandas as pd
+import os
 
 item_routes = Blueprint('items', __name__)
 
 @item_routes.route('/')
 def get_menu():
-    df = pd.read_csv('path/to/menu.csv')
-    return jsonify(df.to_dict(orient='records'))
+    file_path = './mock_restaurant_data.csv'
+    if os.path.isfile(file_path):
+        df = pd.read_csv(file_path)
+        grouped = df.groupby('category')
+
+        menu = {}
+        for name, group in grouped:
+            items = group.drop('category', axis=1).to_dict(orient='records')
+            menu[name] = items
+
+        return jsonify(menu)
+    else:
+        return jsonify({'error': 'File not found'}), 404
 
 @item_routes.route('/<int:id>')
 def item(id):
--- react-app/src/components/MenuPage/MenuPage.js
+++ react-app/src/components/MenuPage/MenuPage.js
@@ -9,85 +9,83 @@ const MenuPage = () => {
   };
 
   const filteredMenu = selectedCategory
-    ? menu.filter((item) => item.category === selectedCategory)
-    : menu;
+    ? menu[selectedCategory]
+    : Object.values(menu).flat();
 
   useEffect(() => {
-    fetch("/api/menu")
+    fetch("/api/items/")
       .then((response) => response.json())
       .then((data) => {
-        const groupedData = data.reduce((acc, item) => {
-          if (!acc[item.category]) {
-            acc[item.category] = [];
-          }
-          acc[item.category].push(item);
-          return acc;
-        }, {});
-        setMenu(groupedData);
+        setMenu(data);
       });
   }, []);
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-start items-center bg-blue-800">
-      <h1 className="text-5xl text-center text-red-500 mt-8">Our Menu</h1>
-      <p className="w-3/4 mx-auto text-center text-white tracking-wider py-3 leading-loose">
+    <div className="flex min-h-screen w-screen flex-col items-center justify-start bg-blue-800">
+      <h1 className="mt-8 text-center text-5xl text-red-500">Our Menu</h1>
+      <p className="mx-auto w-3/4 py-3 text-center leading-loose tracking-wider text-white font-thin">
         * May contain raw or undercooked ingredients. Consuming raw or
         undercooked meats, poultry, seafood, shellfish, or eggs may increase
         your risk of food-borne illness, especially if you have certain medical
         conditions
       </p>
       <span className="py-10">
-        <ul className="flex flex-row space-x-9">
+        <ul className="flex flex-row space-x-9 text-xl text-white">
           <li
-            className={`cursor-pointer ${selectedCategory === "APPETIZERS" ? "text-red-500" : ""}`}
+            className={`cursor-pointer ${selectedCategory === "ALL" ? "text-red-500 underline" : ""}`}
+            onClick={() => handleItemClick("ALL")}
+          >
+            ALL
+          </li>
+          <li
+            className={`cursor-pointer ${selectedCategory === "APPETIZERS" ? "text-red-500 underline" : ""}`}
             onClick={() => handleItemClick("APPETIZERS")}
           >
             APPETIZERS
           </li>
           <li
-            className={`cursor-pointer ${selectedCategory === "SALADS" ? "text-red-500" : ""}`}
+            className={`cursor-pointer ${selectedCategory === "SALADS" ? "text-red-500 underline" : ""}`}
             onClick={() => handleItemClick("SALADS")}
           >
             SALADS
           </li>
           <li
-            className={`cursor-pointer ${selectedCategory === "SOUPS" ? "text-red-500" : ""}`}
+            className={`cursor-pointer ${selectedCategory === "SOUPS" ? "text-red-500 underline" : ""}`}
             onClick={() => handleItemClick("SOUPS")}
           >
             SOUPS
           </li>
           <li
-            className={`cursor-pointer ${selectedCategory === "ENTREES" ? "text-red-500" : ""}`}
+            className={`cursor-pointer ${selectedCategory === "ENTREES" ? "text-red-500 underline" : ""}`}
             onClick={() => handleItemClick("ENTREES")}
           >
             ENTREES
           </li>
           <li
-            className={`cursor-pointer ${selectedCategory === "DESSERTS" ? "text-red-500" : ""}`}
+            className={`cursor-pointer ${selectedCategory === "DESSERTS" ? "text-red-500 underline" : ""}`}
             onClick={() => handleItemClick("DESSERTS")}
           >
             DESSERTS
           </li>
           <li
-            className={`cursor-pointer ${selectedCategory === "DRINKS" ? "text-red-500" : ""}`}
+            className={`cursor-pointer ${selectedCategory === "DRINKS" ? "text-red-500 underline" : ""}`}
             onClick={() => handleItemClick("DRINKS")}
           >
             DRINKS
           </li>
         </ul>
       </span>
-      {Object.entries(menu).map(([category, items]) => (
-        <section key={category}>
-          <h2>{category}</h2>
-          {items.map((item, index) => (
+      <div className="grid grid-cols-3 gap-4 text-white">
+        {filteredMenu &&
+          filteredMenu.map((item, index) => (
             <div key={index}>
+              <h3>{item.category}</h3>
               <h3>{item.name}</h3>
               <p>{item.description}</p>
               <p>{item.price}</p>
             </div>
           ))}
-        </section>
-      ))}
+      </div>
     </div>
   );
 };
