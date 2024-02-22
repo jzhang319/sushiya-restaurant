1d2a033abbd0ea771a93c9689145387be0a55be9
jzhang319 <jzhang319@gmail.com>
2024-02-22 12:53:20 -0500

    merging from jackz, updated navigation bar

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./react-app/src/**/*.{html,js}"],
-  important: true,
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
