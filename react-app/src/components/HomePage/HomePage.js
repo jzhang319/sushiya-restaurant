d3c142fc1b0e13b610e66b5b893a6a6e232df5ba
Ray Yu <yxr686@gmail.com>
2024-02-24 18:02:10 -0500

    update

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,12 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import './HomePage.css'
 
 function HomePage() {
   return (
     <div className='panel-1'>
-      <div>Location</div>
-      <div>Menu</div>
-      <div>Contact Us</div>
+      <Link to="/about-us">
+        <button>Location</button>
+      </Link>
+      <Link to="/menu">
+        <button>Menu</button>
+      </Link>
+      <Link to="/about-us">
+        <button>Contact Us</button>
+      </Link>
+
     </div>
   )
 }
