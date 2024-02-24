ce4e11b3238df44b5e548fc3cf214bf2e20d575e
jzhang319 <jzhang319@gmail.com>
2024-02-24 18:56:33 -0500

    finished linking nav buttons

--- react-app/src/components/Navigation/ChipTab.js
+++ react-app/src/components/Navigation/ChipTab.js
@@ -1,44 +1,39 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { NavLink } from "react-router-dom"; // Import NavLink
 
-const tabs = ["Home", "Menu", "About-Us"];
+const tabs = [
+  { name: "Home", path: "/" },
+  { name: "Menu", path: "/menu" },
+  { name: "About-Us", path: "/about-us" },
+];
 
-const ChipTabs = ({className}) => {
-  const [selected, setSelected] = useState(tabs[0]);
-
+const ChipTabs = ({ className }) => {
   return (
-    <div className={`${className} flex flex-wrap justify-end items-center gap-2 bg-black px-4 py-10`}>
+    <div
+      className={`${className} flex flex-wrap items-center justify-end gap-2 bg-black px-4 py-10`}
+    >
       {tabs.map((tab) => (
-        <Chip
-          text={tab}
-          selected={selected === tab}
-          setSelected={setSelected}
-          key={tab}
-        />
+        <Chip text={tab.name} path={tab.path} key={tab.name} />
       ))}
     </div>
   );
 };
 
-const Chip = ({ text, selected, setSelected }) => {
+const Chip = ({ text, path }) => {
   return (
-    <button
-      onClick={() => setSelected(text)}
-      className={`${
-        selected
-          ? "text-white"
-          : "text-slate-300 hover:bg-slate-700 hover:text-slate-200"
-      } relative rounded-md px-2.5 py-0.5 text-sm transition-colors`}
+    <NavLink
+      exact
+      to={path}
+      activeClassName="text-white bg-gradient-to-r from-violet-600 to-indigo-600" // Add this line
+      className={`relative rounded-md px-2.5 py-0.5 text-sm text-slate-300 transition-colors hover:bg-slate-700 hover:text-slate-200`}
     >
       <span className="relative z-10">{text}</span>
-      {selected && (
-        <motion.span
-          layoutId="pill-tab"
-          transition={{ type: "spring", duration: 0.5 }}
-          className="absolute inset-0 z-0 rounded-md bg-gradient-to-r from-violet-600 to-indigo-600"
-        ></motion.span>
-      )}
-    </button>
+      <motion.span
+        layoutId="pill-tab"
+        transition={{ type: "spring", duration: 0.5 }}
+        className="absolute inset-0 z-0 rounded-md"
+      ></motion.span>
+    </NavLink>
   );
 };
 
