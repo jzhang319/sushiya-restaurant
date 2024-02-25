c775bea26c784eeb3c21c39599122f7ae625bb50
Ray Yu <yxr686@gmail.com>
2024-02-25 17:16:03 -0500

    update

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './HomePage.css'
-
+import image2 from './front-page-2.jpg'
 function HomePage() {
   return (
-    <div className='panel-1'>
-      <Link to="/about-us">
-        <button>Location</button>
-      </Link>
-      <Link to="/menu">
-        <button>Menu</button>
-      </Link>
-      <Link to="/about-us">
-        <button>Contact Us</button>
-      </Link>
+    <div className='homepage'>
+      <div className='panel-1'>
+        <Link to="/about-us">
+          <button>Location</button>
+        </Link>
+        <Link to="/menu">
+          <button>Menu</button>
+        </Link>
+        <Link to="/about-us">
+          <button>Contact Us</button>
+        </Link>
+      </div>
+      <div className='panel-2'>
+        <div>
+          <h1>WELCOME TO</h1>
+          <h1>SUSHI YA</h1>
 
+        </div>
+        <img src={image2}  />
+      </div>
     </div>
+
+
   )
 }
 
