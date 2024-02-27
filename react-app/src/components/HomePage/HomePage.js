c766cd28218315c72eabbf21a174d032b9ec65cf
Ray Yu <yxr686@gmail.com>
2024-02-26 23:51:42 -0500

    update

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import './HomePage.css'
@@ -20,6 +21,13 @@ const center = {
 
 
 function HomePage() {
+
+  const [showInfoWindow, setShowInfoWindow] = useState(false);
+
+  const handleMarkerClick = () => {
+    setShowInfoWindow(true);
+  };
+
   return (
     <div className='homepage'>
       <div className='panel-1'>
@@ -43,21 +51,24 @@ function HomePage() {
       </div>
       <div>
         <LoadScript
-          googleMapsApiKey="AIzaSyCeKDpXRdjmrGdjpU06UsCUmVa0L_MMWlk"
+          googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
         >
           <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
             zoom={10}
           >
-            <InfoWindow position={center}>
-              <div>
-                <h4>Sushi Ya</h4>
-                <p>949 Franklin Ave, Garden City, NY 11530</p>
-                <a href="https://www.google.com/maps/dir//Sushi+Ya/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x89c27d8f3c3feabb:0x53d8e7d6588a9a73?sa=X&ved=2ahUKEwiJotbB4PzvAhW1l-AKHQJiDzQQ9RcwFHoECAYQBA" target="_blank" rel="noopener noreferrer">Get Directions</a>
-              </div>
-            </InfoWindow>
-            <Marker position={center} />
+            <Marker position={center} onClick={handleMarkerClick}>
+              {showInfoWindow && (
+                <InfoWindow onCloseClick={() => setShowInfoWindow(false)}>
+                  <div>
+                    <h4>Sushi Ya</h4>
+                    <p>949 Franklin Ave, Garden City, NY 11530</p>
+                    <a href="https://www.google.com/maps/dir//Sushi+Ya/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x89c27d8f3c3feabb:0x53d8e7d6588a9a73?sa=X&ved=2ahUKEwiJotbB4PzvAhW1l-AKHQJiDzQQ9RcwFHoECAYQBA" target="_blank" rel="noopener noreferrer">Get Directions</a>
+                  </div>
+                </InfoWindow>
+              )}
+            </Marker>
           </GoogleMap>
         </LoadScript>
       </div>
