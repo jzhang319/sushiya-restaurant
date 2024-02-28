5a96e361269514b3c476888c329a92c79fb8f8a2
Ray Yu <yxr686@gmail.com>
2024-02-27 23:39:16 -0500

    update

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -34,17 +34,17 @@ function HomePage() {
       <div className="flex flex-col justify-center items-center space-y-4 w-full h-screen p-6 mt-4 bg-white shadow"
         style={{ backgroundImage: `url(${image1})`, backgroundSize: 'cover' }}>
         <Link to="/about-us" className="btn-blue">
-          <button className="bg-transparent hover:bg-transparent text-white text-2xl font-bold py-2 px-4 rounded shadow-lg">
+          <button className="bg-transparent hover:bg-transparent text-white text-2xl sm:text-3xl font-bold py-2 px-4 rounded shadow-lg">
             Location
           </button>
         </Link>
         <Link to="/menu" className="btn-blue">
-          <button className="bg-transparent hover:bg-transparent text-white text-2xl font-bold py-2 px-4 rounded shadow-lg">
+          <button className="bg-transparent hover:bg-transparent text-white text-2xl sm:text-3xl font-bold py-2 px-4 rounded shadow-lg">
             Menu
           </button>
         </Link>
         <Link to="/about-us" className="btn-blue">
-          <button className="bg-transparent hover:bg-transparent text-white text-2xl font-bold py-2 px-4 rounded shadow-lg">
+          <button className="bg-transparent hover:bg-transparent text-white text-2xl sm:text-3xl font-bold py-2 px-4 rounded shadow-lg">
             Contact Us
           </button>
         </Link>
