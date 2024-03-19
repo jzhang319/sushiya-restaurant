292d28abbf8ab11557a10d14630327f1972cb2b1
jzhang319 <jzhang319@gmail.com>
2024-03-18 23:14:18 -0400

    fixed dependency issues with babel and emailjs, also added images folder to their component folder

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import image1 from './images/image1.jpg'
+import image2 from './images/image2.png'
 
 function HomePage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <div
         className="mt-4 flex h-screen w-full flex-col items-center justify-center space-y-4 bg-white p-6 shadow"
-        // style={{ backgroundImage: `url(${image1})`, backgroundSize: "cover" }}
+        style={{ backgroundImage: `url(${image1})`, backgroundSize: "cover" }}
       >
         <Link to="/about-us" className="btn-blue">
           <button className="rounded bg-transparent px-4 py-2 text-2xl font-bold text-white shadow-lg hover:bg-transparent sm:text-3xl">
@@ -29,7 +31,7 @@ function HomePage() {
           <h1 className="text-4xl font-bold">WELCOME TO</h1>
           <h1 className="text-4xl font-bold">SUSHI YA</h1>
         </div>
-        {/* <img src={image2} alt="" className="w-1/2" /> */}
+        <img src={image2} alt="" className="w-1/2" />
       </div>
     </div>
   );
