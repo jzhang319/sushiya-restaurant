8ea2a6cde180a6e0958665e64ba47e9cd6834303
Ray Yu <yxr686@gmail.com>
2024-03-24 18:36:21 -0400

    email model update

--- app/models/email.py
+++ app/models/email.py
@@ -11,3 +11,4 @@ class Email(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(40))
     email = db.Column(db.String(100),nullable=False, unique=True)
+    phone = db.Column(db.Integer)
