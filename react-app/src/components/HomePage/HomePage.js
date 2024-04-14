4d0e302c8757b89052855f3c965661974f38c716
Ray Yu <yxr686@gmail.com>
2024-04-13 21:49:46 -0400

    frontpagemobilefriendlyupdate

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -26,7 +26,7 @@ function HomePage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-col justify-center items-center space-y-4 w-full h-screen p-6 bg-white shadow"
+      <div className="flex flex-col justify-center items-center space-y-4 w-full h-screen p-6 bg-white shadow sm:space-y-0 sm:flex-row sm:space-x-4"
         style={{ backgroundImage: `url(${image1})`, backgroundSize: 'cover' }}>
         <Link to="/about-us" className="btn-blue">
           <button className="bg-transparent hover:bg-transparent text-white text-2xl sm:text-3xl font-bold py-2 px-4 rounded shadow-lg slide-up">
@@ -44,12 +44,12 @@ function HomePage() {
           </button>
         </Link>
       </div>
-      <div className="flex justify-around items-center w-full bg-white shadow p-6 mt-4">
+      <div className="flex flex-col justify-around items-center w-full bg-white shadow p-6 mt-4 sm:flex-row">
         <div>
           <h1 className="text-4xl font-bold">WELCOME TO</h1>
           <h1 className="text-4xl font-bold">SUSHI YA</h1>
         </div>
-        <img src={image2} alt='' className="w-1/2" />
+        <img src={image2} alt='' className="w-full sm:w-1/2" />
       </div>
       <EmailRegistration />
     </div>
